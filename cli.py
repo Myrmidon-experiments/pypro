8624a4a6a06483e2b765ff612d3a220ed76a0467
cactus <correa.francisco.21@gmail.com>
2016-08-16 00:23:22 -0300

    Add TODO FILE, cli almost finished

--- cli.py
+++ cli.py
@@ -6,6 +6,20 @@ usage = "%(prog)s [-h] [-v] <command> [<args>]"
 version = "0.0.1"
 
 
+def init(args):
+    print("First \n%s" % args.name)
+    print("scheme %d" % args.scheme)
+    print("vcs %s" % args.vcs)
+    print("venv %s" % args.venv)
+
+
+def analize(args):
+    print("Second")
+    print("save %d" % args.save)
+    print("path %s" % args.path)
+    print("novcs", args.novcs)
+
+
 def get_args():
     parser = argparse.ArgumentParser(prog='pypro',
                                      add_help=False)
@@ -16,30 +30,32 @@ def get_args():
                         help="show the current version")
     subparsers = parser.add_subparsers()
 
-    sp_analize = subparsers.add_parser('analize', help="Analize help")
+    sp_analize = subparsers.add_parser('analize',
+                                       help="%(prog)s analize --help")
     sp_analize.add_argument('path', nargs='?', default=os.getcwd(),
-                            help="Full or relative path. \
+                            help="full or relative path. \
                             If not given cwd is default")
     sp_analize.add_argument('--save', nargs='?', const=1, type=int,
-                            metavar="SCHEME", help="Save on scheme file")
-    sp_analize.add_argument('--no-vcs', action="store_true",
-                            help="Analize ignoring the version control system")
+                            metavar="SCHEME", help="save on scheme file")
+    sp_analize.add_argument('--no-vcs', action="store_true", dest="novcs",
+                            help="analize ignoring the version control system")
+    sp_analize.set_defaults(func=analize)
 
-    sp_init = subparsers.add_parser('init', help="Init help")
-    sp_init.add_argument('name', help="Create a new project with a given name")
+    sp_init = subparsers.add_parser('init', help="pypro init --help")
+    sp_init.add_argument('name',
+                         help="initialize a new project with a given name")
     sp_init.add_argument('--venv', nargs='*', metavar='venv_args',
-                         help="New project", required=False)
+                         help="create a virtualenv with the same project name")
     sp_init.add_argument('--vcs', nargs='*', metavar="vcs_args",
-                         help="Choose a vcs and give a path for ignorefile")
+                         help="choose a vcs and give a path for ignorefile")
     sp_init.add_argument('-s', '--scheme', nargs='?', const=1, type=int,
-                         help="Select scheme number. Default 1")
+                         help="select scheme number. Default 1")
+    sp_init.set_defaults(func=init)
 
     return parser.parse_args()
-    """parser.print_help()
-    print('\n###\n')
-    sp_init.print_help()
-    print('\n###\n')
-    sp_analize.print_help()
-    """
 
-get_args()
+
+def main(args=get_args()):
+    args.func(args)
+
+main()
